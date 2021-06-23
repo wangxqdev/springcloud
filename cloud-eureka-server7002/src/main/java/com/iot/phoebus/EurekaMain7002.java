a730ab56c44fa6ab36b0b713a149131fec799fd9
wangxinquan <xinquan.w@phoebus-iot.com>
2021-06-23 15:29:25 +0800

    feature：eureka集群

---
+++ cloud-eureka-server7002/src/main/java/com/iot/phoebus/EurekaMain7002.java
@@ -0,0 +1,18 @@
+package com.iot.phoebus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @auhtor dell
+ * @date 2021/6/23
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}
