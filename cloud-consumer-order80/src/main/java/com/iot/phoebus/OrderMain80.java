c948bad999090ce7755088d008bac1e443158de8
wangxinquan <xinquan.w@phoebus-iot.com>
2021-06-23 14:33:30 +0800

    feature：80入住集群

--- cloud-consumer-order80/src/main/java/com/iot/phoebus/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/iot/phoebus/OrderMain80.java
@@ -2,12 +2,14 @@ package com.iot.phoebus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author xinquan.w
  * @date 2021/6/20
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
 
     public static void main(String[] args) {
