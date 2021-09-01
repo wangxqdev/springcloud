8e11ee124e9142693d31f354a3aca7d0ac0a60c2
wangxinquan <xinquan.w@phoebus-iot.com>
2021-09-01 13:42:38 +0800

    feature：Hystrix熔断案例

--- cloud-provider-hystrix-payment8001/src/main/java/com/iot/phoebus/controller/PaymentController.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/iot/phoebus/controller/PaymentController.java
@@ -1,5 +1,6 @@
 package com.iot.phoebus.controller;
 
+import cn.hutool.core.util.IdUtil;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
@@ -40,8 +41,29 @@ public class PaymentController {
         return response;
     }
 
+    @HystrixCommand(fallbackMethod = "paymentCircuitBreakerFallback", commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.enabled", value = "true"),// 是否开启断路器
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),// 请求次数
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000"), // 时间窗口期
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60"),// 失败率达到多少后跳闸
+    })
+    @GetMapping("/hystrix/circuit/{id}")
+    public String paymentCircuitBreaker(@PathVariable("id") Long id) {
+        if (id < 0) {
+            throw new RuntimeException("id「" + id + "」不能为负数");
+        }
+        String serialNumber = IdUtil.simpleUUID();
+        return Thread.currentThread().getName() + " paymentCircuitBreaker(id=" + id + ") serialNumber=" + serialNumber;
+    }
+
     private String getPaymentByIdFallback(Long id, Integer timeout) {
         String response = Thread.currentThread().getName() + " getPaymentByIdFallback(id=" + id + ",timeout=" + timeout + ")";
+        log.warn(response);
+        return response;
+    }
+
+    private String paymentCircuitBreakerFallback(Long id) {
+        String response = Thread.currentThread().getName() + " getPaymentByIdFallback(id=" + id + ")";
         log.warn(response);
         return response;
     }
